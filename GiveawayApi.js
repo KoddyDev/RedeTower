396863a23269cc86d7b2334105fdfbe3ed32665b
KoddyDev <73896941+KoddyDev@users.noreply.github.com>
2021-09-02 17:11:09 -0300

    Update GiveawayApi.js

--- GiveawayApi.js
+++ GiveawayApi.js
@@ -7,7 +7,7 @@ const { Message, Channel, Emoji } = require("discord.js");
 exports.finish = async (msg, emoji) => {
     const reaction = await msg.reactions.cache.get(emoji)
     console.log(reaction.count)
-    if(reaction.count < 1) return "Não foi possivel pegar o usuario"
+    if(reaction.count < 1) return "Não existem usuarios suficientes para o sorteio ser realizado."
     let usuariosNF = await reaction.users.fetch()
     let usuariosF = await usuariosNF.filter(f2 => !f2.bot)
     return usuariosF.random()
